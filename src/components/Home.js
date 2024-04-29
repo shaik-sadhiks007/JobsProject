52d8b5a029119ccc341e7d9087c182ea2f43a12d
shaik-sadhiks007 <shaik.sadhiks007@gmail.com>
2024-04-29 19:09:47 +0530

    order changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,13 +18,13 @@ export default function Home() {
                     <Banner />
                     <div className="container my-5">
                         <div className="row">
-                            <div className="col-lg-3 col-md-5 col-sm-12 order-md-1">
+                            <div className="col-lg-3 col-md-5 col-sm-12 order-1 order-lg-1 order-md-1">
                                 <Filters />
                             </div>
-                            <div className="col-lg-6 col-md-12 col-sm-12 mt-md-0 mt-sm-5 order-md-3">
+                            <div className="col-lg-6 col-md-12 col-sm-12 mt-md-0 mt-sm-5 order-2 order-lg-2 order-md-3">
                                 <Jobs />
                             </div>
-                            <div className="col-lg-3 col-md-7 col-sm-12 mt-sm-0 mt-md-5 order-md-2">
+                            <div className="col-lg-3 col-md-7 col-sm-12 mt-sm-0 mt-md-5 order-3 order-lg-3 order-md-2">
                                 <Contact />
                             </div>
                         </div>
